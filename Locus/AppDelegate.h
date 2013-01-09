446ca612b94739ad1724faf61476678414393ece
Barry Alexander <barry.alexander@gmail.com>
2013-01-08 20:28:16 -0800

    Initial Commit

---
+++ Locus/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Locus
+//
+//  Created by barry alexander on 1/8/13.
+//  Copyright (c) 2013 barry alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Locus/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Locus
+//
+//  Created by barry alexander on 1/8/13.
+//  Copyright (c) 2013 barry alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
