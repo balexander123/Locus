502f7d542a9534e4c4a7c886929529d1eaf4bf16
Barry Alexander <barry.alexander@gmail.com>
2013-04-23 11:46:15 -0700

    commit before some refactors
    1) replace app delegate ‘globals’ db url, database name with
    singletons.  Yeah, you heard right.
    2) refactor create view, compute view methods of cb helper to pass in
    view name parameter
    3) refactor couchbase helper to init with db url and database name

---
+++ Specs/CampusListSpec.mm
@@ -0,0 +1,16 @@
+#import "CampusList.h"
+
+using namespace Cedar::Matchers;
+using namespace Cedar::Doubles;
+
+SPEC_BEGIN(CampusListSpec)
+
+describe(@"CampusList", ^{
+    __block CampusList *model;
+
+    beforeEach(^{
+
+    });
+});
+
+SPEC_END
