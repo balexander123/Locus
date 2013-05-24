e242a9a6da4e6c6e97f553b90a4a6e31d9590ea0
Barry Alexander <barry.alexander@gmail.com>
2013-05-23 21:58:25 -0700

    Refactoring
    Got rid of CampusList in favor of using a protocol on Campus object

---
+++ Locus/CouchCRUD.h
@@ -0,0 +1,13 @@
+//
+//  CouchCRUD.h
+//  Locus
+//
+//  Created by barry alexander on 5/23/13.
+//  Copyright (c) 2013 barry alexander. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol CouchCRUD <NSObject>
+
+@end
---
+++ Specs/CampusSpec.mm
@@ -0,0 +1,16 @@
+#import "Campus.h"
+
+using namespace Cedar::Matchers;
+using namespace Cedar::Doubles;
+
+SPEC_BEGIN(CampusSpec)
+
+describe(@"Campus", ^{
+    __block Campus *model;
+
+    beforeEach(^{
+
+    });
+});
+
+SPEC_END
